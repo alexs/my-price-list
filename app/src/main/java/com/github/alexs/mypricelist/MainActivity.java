bab47977237d862c663da7f99b79a094405f4d4b
Alex S <alexs1010@gmail.com>
2015-03-23 18:40:52 -0600

    Start project with initial activity

---
+++ app/src/main/java/com/github/alexs/mypricelist/MainActivity.java
@@ -0,0 +1,50 @@
+package com.github.alexs.mypricelist;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private String[] products = {"Ubuntu", "Android", "iOS", "Windows", "Mac OSX",
+            "Google Chrome OS", "Debian", "Mandriva", "Solaris", "Unix", "Ubuntu", "Android", "iOS", "Windows", "Mac OSX",
+            "Google Chrome OS", "Debian", "Mandriva", "Solaris", "Unix"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView products_list = (ListView)findViewById(R.id.product_list);
+        ArrayAdapter<String> productsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, products);
+        products_list.setAdapter(productsAdapter);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
